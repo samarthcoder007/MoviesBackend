c913c70866c887f9f8d021183f39e44313da11fa
samarthcoder007 <93431080+samarthcoder007@users.noreply.github.com>
2024-07-20 13:00:20 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ export default function App(){
 
    return (<>
       <h1 id="header">Movies API</h1>
-      <h3>To learn how to use, <a href="https://github.com/samarthcoder007/MoviesFrontend/blob/main/README.md">Click here</a></h3>
+      <h3 id="header">To learn how to use, <a href="https://github.com/samarthcoder007/MoviesFrontend/blob/main/README.md">Click here</a></h3>
       <Movie/>
     </>
    )
